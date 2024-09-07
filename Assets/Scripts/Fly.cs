ab20348e2f301025dc719b856e956d704c49853e
Carlos-Reinheimer <carlosgreinheimer@gmail.com>
2024-09-07 15:48:38 -0300

    add sprites; bird fly and pipe spawn behavior

---
+++ Assets/Scripts/Fly.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+
+    [SerializeField] private float _velocity = 1.5f;
+    [SerializeField] private float _rotationSpeed = 10f;
+
+    private Rigidbody2D _rb;
+
+    private void Start() {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            _rb.velocity = Vector2.up * _velocity;
+        }
+    }
+
+    private void FixedUpdate() {
+        transform.rotation = Quaternion.Euler(0, 0, _rb.velocity.y * _rotationSpeed);
+    }
+}
